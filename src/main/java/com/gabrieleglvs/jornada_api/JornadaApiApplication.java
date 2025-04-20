fa8b33c93a87f572061d6ae92b0039e32c1dcdbe
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-20 00:40:58 -0300

    primeiro commit

---
+++ src/main/java/com/gabrieleglvs/jornada_api/JornadaApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabrieleglvs.jornada_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornadaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornadaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrieleglvs/jornada_api/JornadaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrieleglvs.jornada_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
