387acbb768fc71e07ce7047ee852058e156157ec
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-20 11:54:43 -0300

    feat<model>: criar entidade Depoimento

--- src/main/java/com/gabrieleglvs/jornada_api/model/Depoimento.java
+++ src/main/java/com/gabrieleglvs/jornada_api/model/Depoimento.java
@@ -1,2 +1,29 @@
-package com.gabrieleglvs.jornada_api.model;public class Depoimento {
+package com.gabrieleglvs.jornada_api.model;
+
+import com.gabrieleglvs.jornada_api.dto.depoimento.DadosCadastrarDepoimento;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity(name = "Depoimento")
+@Table(name = "depoimentos")
+public class Depoimento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String foto;
+    private String depoimento;
+    private String nome;
+
+    public Depoimento(DadosCadastrarDepoimento dados) {
+        this.foto = dados.foto();
+        this.depoimento = dados.depoimento();
+        this.nome = dados.nome();
+    }
 }
