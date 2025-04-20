07287736b85b43b6102bbda9ea1062e711bf159e
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-20 12:01:30 -0300

    feat<service>: criar regra para cadastrarDepoimento da entidade Depoimento

--- src/main/java/com/gabrieleglvs/jornada_api/service/DepoimentoService.java
+++ src/main/java/com/gabrieleglvs/jornada_api/service/DepoimentoService.java
@@ -1,2 +1,26 @@
-package com.gabrieleglvs.jornada_api.service;public class DepoimentoService {
+package com.gabrieleglvs.jornada_api.service;
+
+import com.gabrieleglvs.jornada_api.dto.depoimento.DadosCadastrarDepoimento;
+import com.gabrieleglvs.jornada_api.dto.depoimento.DadosDetalhamentoDepoimento;
+import com.gabrieleglvs.jornada_api.model.Depoimento;
+import com.gabrieleglvs.jornada_api.repository.IDepoimentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class DepoimentoService {
+    @Autowired
+    private IDepoimentoRepository iDepoimentoRepository;
+
+    public ResponseEntity cadastrarDepoimento(DadosCadastrarDepoimento dados,
+                                              UriComponentsBuilder uriBuilder) {
+        Depoimento depoimento = new Depoimento(dados);
+        iDepoimentoRepository.save(depoimento);
+
+        var uri = uriBuilder.path("/depoimentos/{id}").buildAndExpand(depoimento.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoDepoimento(depoimento));
+    }
 }
