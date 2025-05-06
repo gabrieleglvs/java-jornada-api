25bb0eb05b59a7721a5a955ee3ead6af55cb3a57
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-06 15:54:19 -0300

    feat<controller>: criar endpoint para listar todos os destinos

--- src/main/java/com/gabrieleglvs/jornada_api/controller/DestinoController.java
+++ src/main/java/com/gabrieleglvs/jornada_api/controller/DestinoController.java
@@ -1,2 +1,22 @@
-package com.gabrieleglvs.jornada_api.controller;public class DestinoController {
+package com.gabrieleglvs.jornada_api.controller;
+
+import com.gabrieleglvs.jornada_api.service.DepoimentoService;
+import com.gabrieleglvs.jornada_api.service.DestinoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class DestinoController {
+
+    @Autowired
+    private DestinoService service;
+
+    @GetMapping("destinos")
+    public ResponseEntity listarDestinos(){
+        return service.listarDestinos();
+    }
 }
