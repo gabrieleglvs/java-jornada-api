349a6b87cd349ddee7aa58881f738ddd66bd88a4
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-06 15:55:48 -0300

    feat<repository>: criar repositorio para entidade Destino

--- src/main/java/com/gabrieleglvs/jornada_api/repository/IDestinoRepository.java
+++ src/main/java/com/gabrieleglvs/jornada_api/repository/IDestinoRepository.java
@@ -1,2 +1,7 @@
-package com.gabrieleglvs.jornada_api.repository;public interface IDestinoRepository {
+package com.gabrieleglvs.jornada_api.repository;
+
+import com.gabrieleglvs.jornada_api.model.Destino;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IDestinoRepository extends JpaRepository<Destino, Long> {
 }
