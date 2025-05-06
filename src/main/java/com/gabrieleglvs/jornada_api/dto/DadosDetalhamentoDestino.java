51587853de3df304d876cc526563ec4c1061f408
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-06 15:58:01 -0300

    feat<dto>: criar DTO para transportar dados de detalhamento entidade Destino

--- src/main/java/com/gabrieleglvs/jornada_api/dto/DadosDetalhamentoDestino.java
+++ src/main/java/com/gabrieleglvs/jornada_api/dto/DadosDetalhamentoDestino.java
@@ -1,2 +1,12 @@
-package com.gabrieleglvs.jornada_api.dto;public class DadosDetalhamentoDestino {
+package com.gabrieleglvs.jornada_api.dto;
+
+import com.gabrieleglvs.jornada_api.model.Destino;
+
+public record DadosDetalhamentoDestino(String foto,
+                                      String nome,
+                                      double preco) {
+    public DadosDetalhamentoDestino(Destino destino) {
+        this(destino.getFoto(), destino.getNome(), destino.getPreco());
+
+    }
 }
