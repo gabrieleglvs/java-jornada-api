5d8001ddf06bd25df6362b913fe598fce2a7c529
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-06 16:33:48 -0300

    feat<service>: criar regra para cadastrar um destino

--- src/main/java/com/gabrieleglvs/jornada_api/service/DestinoService.java
+++ src/main/java/com/gabrieleglvs/jornada_api/service/DestinoService.java
@@ -1,11 +1,13 @@
 package com.gabrieleglvs.jornada_api.service;
 
-import com.gabrieleglvs.jornada_api.dto.DadosDetalhamentoDestino;
+import com.gabrieleglvs.jornada_api.dto.destino.DadosCadastrarDestino;
+import com.gabrieleglvs.jornada_api.dto.destino.DadosDetalhamentoDestino;
 import com.gabrieleglvs.jornada_api.model.Destino;
 import com.gabrieleglvs.jornada_api.repository.IDestinoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,6 +16,15 @@ import java.util.stream.Collectors;
 public class DestinoService {
     @Autowired
     private IDestinoRepository iDestinoRepository;
+
+    public ResponseEntity cadastrarDestino(DadosCadastrarDestino dados, UriComponentsBuilder uriBuilder) {
+        Destino destino = new Destino(dados);
+        iDestinoRepository.save(destino);
+
+        var uri = uriBuilder.path("/destinos/{id}").buildAndExpand(destino.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoDestino(destino));
+    }
 
     public ResponseEntity listarDestinos() {
         List<Destino> destinoList = iDestinoRepository.findAll();
