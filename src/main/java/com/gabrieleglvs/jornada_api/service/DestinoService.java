bbc57534d6a33ecdffda81d188029ae8dfd1bf25
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-06 15:56:46 -0300

    feat<service>: criar regra para listar todos os destinos

--- src/main/java/com/gabrieleglvs/jornada_api/service/DestinoService.java
+++ src/main/java/com/gabrieleglvs/jornada_api/service/DestinoService.java
@@ -1,2 +1,25 @@
-package com.gabrieleglvs.jornada_api.service;public class DestinoService {
+package com.gabrieleglvs.jornada_api.service;
+
+import com.gabrieleglvs.jornada_api.dto.DadosDetalhamentoDestino;
+import com.gabrieleglvs.jornada_api.model.Destino;
+import com.gabrieleglvs.jornada_api.repository.IDestinoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class DestinoService {
+    @Autowired
+    private IDestinoRepository iDestinoRepository;
+
+    public ResponseEntity listarDestinos() {
+        List<Destino> destinoList = iDestinoRepository.findAll();
+        var destinos = destinoList.stream()
+                .map(destino -> new DadosDetalhamentoDestino(destino))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(destinos);
+    }
 }
