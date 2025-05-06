e8e587d811098edcbf3df6017b187b2576e455bf
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-06 15:48:45 -0300

    feat<migration>: criar tabela destinos na database

--- src/main/resources/db/migration/V2__create-table-destinos.sql
+++ src/main/resources/db/migration/V2__create-table-destinos.sql
@@ -2,7 +2,7 @@ CREATE TABLE destinos (
     id bigint not null auto_increment,
     foto VARCHAR(255),
     nome VARCHAR(255),
-    preco VARCHAR(510),
+    preco DECIMAL(10, 2),
 
     PRIMARY KEY(id)
 );
