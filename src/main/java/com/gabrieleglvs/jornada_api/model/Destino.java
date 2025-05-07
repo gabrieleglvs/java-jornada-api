e31e6813f3de0d212ab9ce9485087eea33cdaabe
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-07 15:23:13 -0300

    feat<model>: criar metodo atualizarDestino na entidade Destino

--- src/main/java/com/gabrieleglvs/jornada_api/model/Destino.java
+++ src/main/java/com/gabrieleglvs/jornada_api/model/Destino.java
@@ -1,5 +1,6 @@
 package com.gabrieleglvs.jornada_api.model;
 
+import com.gabrieleglvs.jornada_api.dto.destino.DadosAtualizarDestino;
 import com.gabrieleglvs.jornada_api.dto.destino.DadosCadastrarDestino;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -19,11 +20,23 @@ public class Destino {
     private Long id;
     private String foto;
     private String nome;
-    private double preco;
+    private Double preco;
 
     public Destino(DadosCadastrarDestino dados) {
         this.foto = dados.foto();
         this.nome = dados.nome();
         this.preco = dados.preco();
+    }
+
+    public void atualizarDestino(DadosAtualizarDestino dados) {
+        if(dados.foto() != null) {
+            this.foto = dados.foto();
+        }
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.preco() != null) {
+            this.preco = dados.preco();
+        }
     }
 }
