c5ac98903b1f797c92a581ca27a959bc201e5ced
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-08 16:53:36 -0300

    feat<repository>: consultar destinos por nome na database

--- src/main/java/com/gabrieleglvs/jornada_api/repository/IDestinoRepository.java
+++ src/main/java/com/gabrieleglvs/jornada_api/repository/IDestinoRepository.java
@@ -1,7 +1,12 @@
 package com.gabrieleglvs.jornada_api.repository;
 
+import com.gabrieleglvs.jornada_api.dto.destino.DadosDetalhamentoDestino;
 import com.gabrieleglvs.jornada_api.model.Destino;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface IDestinoRepository extends JpaRepository<Destino, Long> {
+    List<Destino> findAllByNome(String nome);
 }
